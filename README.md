eb1d3a7f3e1780f8b202090e87adbb77ffbce45b
DrunkenPandaa <fifahunter1910@gmail.com>
2020-03-07 14:33:15 +0100

    New Files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Supreme-Bot
-a bot
---
+++ index.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NjQ5MzQyMTgyMDgzMjY0NTIy.XeAfcg.iSiETz6244MrE4JQ3NW6sPYwMZM';
+
+bot.login(process.env.token);
+
+const PREFIX = '+';
+
+bot.on('ready', () =>{
+    console.log('Online!')
+
+})
+
+bot.on('message', message=>{
+
+        let args = message.content.substring(PREFIX.length).split(" ");
+
+        switch(args[0]){
+            case 'ping':
+                message.channel.sendMessage('pong!');
+                break;
+
+            case 'Supreme':
+                message.channel.sendMessage('SUPREME FOREVER');
+                break;
+
+            case 'info':
+                if(args[1] === 'Tobi'){
+                    message.channel.sendMessage('Tobi is Supreme!');
+                }else{
+                    message.channel.sendMessage('yo wtf, invalid args!!')
+                }
+            break;
+            case 'clear':
+            if(!args[1]) return message.reply('yo wtf!! define second arg')
+            message.channel.bulkDelete(args[1]);
+                    message.channel.sendMessage('Deleted a few messages for you!')
+            break;
+
+            case 'embed':
+
+                const embed = new Discord.RichEmbed()
+                .setTitle('User Information')
+                .addField('Username + #', message.member.user.tag, true)
+                .addField('Account Age', message.member.user.createdAt,true)
+                .addField('Current Server', message.guild.name, true)
+                .addField('Discord Univeral ID', message.member.user.id, true)
+                .setThumbnail(message.author.avatarURL)
+                .setFooter('Chatters District :mad:')
+                .setColor(0xF1C40F)
+                message.channel.sendEmbed(embed);
+            break;
+
+            case 'myinfo':
+                message.channel.sendMessage('My Info test')
+        }
+});
